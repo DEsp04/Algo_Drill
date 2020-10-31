c802c124cc4fabf41d0abe667bb3dd8932193e75
david espinal <despinal0425@gmail.com>
2020-10-31 09:55:59 -0400

    worked on an algo

--- algo_practice.js
+++ algo_practice.js
@@ -123,3 +123,39 @@ function number(array) {
 
   return arr;
 }
+
+//7th Algo
+/*
+The Western Suburbs Croquet Club has two categories of membership, 
+Senior and Open. They would like your help with an application form that 
+will tell prospective members which category they will be placed.
+
+To be a senior, a member must be at least 55 years old and have a handicap 
+greater than 7. In this croquet club, handicaps range from -2 to +26; the 
+better the player the lower the handicap.
+
+Input
+
+Input will consist of a list of lists containing two items each. Each 
+list contains information for a single potential member. Information 
+consists of an integer for the person's age and an integer for the person's 
+handicap.
+*/
+function openOrSenior(data){
+
+  let arr = []
+  
+  for(let i = 0; i < data.length; i++){
+    for(let j = 1; j < data[i].length; j++){
+
+      if(data[i][j - 1] >= 55 && data[i][j] > 7){
+        arr.push('Senior');
+      } else {
+        arr.push('Open');
+      }
+    }
+  }
+  return arr;
+}
+
+openOrSenior([[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]);
