ae91419707af6c4ee6491fa2d161c952c6d70c18
david espinal <despinal0425@gmail.com>
2020-11-07 10:14:13 -0500

    study algo 8

--- algo_practice.js
+++ algo_practice.js
@@ -159,3 +159,47 @@ function openOrSenior(data){
 }
 
 openOrSenior([[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]);
+
+/*
+The new "Avengers" movie has just been released! There are a lot of people 
+at the cinema box office standing in a huge line. Each of them has a 
+single 100, 50 or 25 dollar bill. An "Avengers" ticket costs 25 dollars.
+
+Vasya is currently working as a clerk. He wants to sell a ticket to every 
+single person in this line.
+
+Can Vasya sell a ticket to every person and give change if he initially 
+has no money and sells the tickets strictly in the order people queue?
+
+Return YES, if Vasya can sell a ticket to every person and give change with the 
+bills he has at hand at that moment. Otherwise return NO.
+*/
+function tickets(peopleInLine) {
+  debugger
+    let bills = [0, 0, 0]
+    for (let i = 0; i < peopleInLine.length; i++) {
+      switch (peopleInLine[i]) {
+        case 25:
+          bills[0]++
+          break
+          
+        case 50:
+          bills[0]--
+          bills[1]++
+          break
+          
+        case 100:
+          bills[1] ? bills[1]-- : bills[0] -= 2
+          bills[0]--
+          break
+      }
+      
+      if (bills[0] < 0) {
+        return 'NO'
+      }
+    }
+    
+    return 'YES'
+}
+  
+tickets([25,25,25,100,25,25,25,100,25,25,50,100,25,50,25,100,25,50,25,100 ]);
